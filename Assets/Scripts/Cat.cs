7510e0a41319c8f53c37bfe3c1d7817bc7480739
Michelle Wong <michellewong.nma@gmail.com>
2015-04-25 20:15:29 -0700

    cat dies
    cat dies on click
    creates blood particle effect

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+	public GameObject deathParticles;
+	public int health;
+	//private Vector3 spawn;
+
+	// Use this for initialization
+	void Start () {
+		//spawn = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && health > 0) {
+			health--;
+		}
+
+		if (Input.GetMouseButtonDown(0) && (health ==0))
+		{
+			Instantiate(deathParticles, transform.position, Quaternion.identity);
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public int currentscore;
+	//public int highscore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time == 2) {
+			Vector3 rand_pos = new Vector3(Random.Range(-500,500), Random.Range(-250,250), 0);
+			Cat = Instantiate(Cat, rand_pos, Quaternion.identity);
+		}
+	}
+}
